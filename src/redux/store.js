1f609fa5398af9c5d393812a19ac0fb31c8535a5
Mirzohid22 <smm2226fstd@gmail.com>
2021-12-02 22:22:53 +0500

    fixed bug with deployment

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
 const middlewares = [];
-if(process.env.NODE_ENV == "development") {
+if(process.env.NODE_ENV === "development") {
     middlewares.push(logger);
 }
 
