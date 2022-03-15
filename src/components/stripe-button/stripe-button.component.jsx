efd1e7715eb5768627a99fb6ba89e57e88ccae3e
Mirzohid Salimov <smm2226fstd@gmail.com>
2022-03-15 14:10:12 +0500

    removed backend in this repository

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
-import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -8,21 +7,22 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51K2DlTAtWvmZ5594zvkkpbj8lENZY0X5REx0gnLTlcg9OQD2kyfzzddeJZIMYSvJFFoqTbS91HjdxCkCg6RG2k4G00ImOkNTVP";
 
   const onToken = (token) => {
-    axios({
-      url: "http://localhost:5000/payment",
-      method: "POST",
-      amount: priceForStripe,
-      token: token,
-    })
-      .then((response) => {
-        alert("Payment Succesful!");
-      })
-      .catch((error) => {
-        console.log("Payment error: " + JSON.parse(error));
-        alert(
-          "There was an issue with your payment, please sure you use the  provided credit cart."
-        );
-      });
+    alert("Payment succesful: We are in business now");
+    // axios({
+    //   url: "http://localhost:5000/payment",
+    //   method: "POST",
+    //   amount: priceForStripe,
+    //   token: token,
+    // })
+    //   .then((response) => {
+    //     alert("Payment Succesful!");
+    //   })
+    //   .catch((error) => {
+    //     console.log("Payment error: " + JSON.parse(error));
+    //     alert(
+    //       "There was an issue with your payment, please sure you use the  provided credit cart."
+    //     );
+    //   });
   };
 
   return (
